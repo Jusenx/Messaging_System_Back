bc9d29a67c05694b6213e36c1ac332d446b8300e
Jusenx <josedantas2408@gmail.com>
2026-09-08 18:43:54 -0300

    feat: commit inicial

---
+++ src/main/java/com/balthazar/Messaging_System/MessagingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.balthazar.Messaging_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingSystemApplication.class, args);
+	}
+
+}
