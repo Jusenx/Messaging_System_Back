b22b0f8612343209bf709d0d60b35fee3e14e0e2
Jusenx <josedantas2408@gmail.com>
2026-09-08 18:46:53 -0300

    feat: MessageService base

---
+++ src/main/java/com/balthazar/Messaging_System/message/service/MessageService.java
@@ -0,0 +1,38 @@
+package com.balthazar.Messaging_System.message.service;
+
+import com.balthazar.Messaging_System.message.dto.mapper.MessageMapper;
+import com.balthazar.Messaging_System.message.dto.request.MessageRequest;
+import com.balthazar.Messaging_System.message.dto.response.MessageResponse;
+import com.balthazar.Messaging_System.message.entity.Message;
+import com.balthazar.Messaging_System.message.enums.StatusEnum;
+import com.balthazar.Messaging_System.message.provider.factory.ProviderFactory;
+import com.balthazar.Messaging_System.message.repository.MessageRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class MessageService {
+
+    private final MessageRepository repository;
+    private final MessageMapper mapper;
+    private final ProviderFactory factory;
+
+    public MessageResponse create(MessageRequest request) {
+
+        Message message = mapper.toEntity(request);
+        message.setStatus(StatusEnum.PENDING);
+        Message savedMessage = repository.save(message);
+
+        return mapper.toResponse(savedMessage);
+
+    }
+
+    public MessageResponse getById(UUID id){
+        Message message = repository.getById(id);
+        return mapper.toResponse(message);
+    }
+
+}
